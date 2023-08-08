3f19f1d31092d836563a3b8e8c325e0c9b5558d3
cartond <cartond93@gmail.com>
2023-08-07 23:00:46 -0400

    Plan it out

--- src/script.ts
+++ src/script.ts
@@ -2,31 +2,43 @@
 // we detect a callback from Spotify by checking for the hash fragment
 import { redirectToAuthCodeFlow, getAccessToken } from "./authCodeWithPkce";
 
-const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
-const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET
+const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+const SPOTIFY_CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET
 
-if (!CLIENT_ID) {
-    // In the real world this would be an API/library error,
-    //   but for this demo let's just `alert` and stop further execution
-    alert("Missing CLIENT_ID env variable")
-    throw new Error("Missing CLIENT_ID env variable");
-}
-if (!CLIENT_SECRET) {
-    // In the real world this would be an API/library error,
-    //   but for this demo let's just `alert` and stop further execution
-    alert("Missing CLIENT_SECRET env variable")
-    throw new Error("Missing CLIENT_SECRET env variable");
-}
+const code =  "AQDDZBpO_x8yeXvDsjKfSJJBt9Ze7HrRoOT2VpqrIUtqDgs6CL35ULNYgXalQtEN4AGgSnDrJW-mmui452qkcOEkdKo8HzdbbCKrPP6gKAs3E5uinkpAS0AHbq2saTtjGtsxji9ukxKPbPKwoK9SYB8ZdrV8a4cTm5-Mlr8h6STrpOl-spzY0H9DfOvgcY373Y6k-7BSMLb9QsjAE72Ic3RGqoHXbd5o3wDPRvQ3C7vRiop6iAiho4YDcPT5YBbvt3e8vnaTo5HnqKY1OSEO666g7gqbBJVF0Q"
 
-const params = new URLSearchParams(window.location.search);
-const code = params.get("code");
+// search setlist.fm for artist
+// search artist for concerts
+// scrape song list from single concert
+// attempt spotify auth
+// create playlist
+// add songs to playlist
 
-if (!code) {
-    redirectToAuthCodeFlow(CLIENT_ID);
-} else {
-    const accessToken = await getAccessToken(CLIENT_ID, code);
-    const profile = await fetchProfile(accessToken);
-    populateUI(profile);
+
+async function attemptAuthentication() {
+    if (!SPOTIFY_CLIENT_ID) {
+        // In the real world this would be an API/library error,
+        //   but for this demo let's just `alert` and stop further execution
+        alert("Missing SPOTIFY_CLIENT_ID env variable")
+        throw new Error("Missing SPOTIFY_CLIENT_ID env variable");
+    }
+    if (!SPOTIFY_CLIENT_SECRET) {
+        // In the real world this would be an API/library error,
+        //   but for this demo let's just `alert` and stop further execution
+        alert("Missing SPOTIFY_CLIENT_SECRET env variable")
+        throw new Error("Missing SPOTIFY_CLIENT_SECRET env variable");
+    }
+
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (!code) {
+        redirectToAuthCodeFlow(SPOTIFY_CLIENT_ID);
+    } else {
+        const accessToken = await getAccessToken(SPOTIFY_CLIENT_ID, code);
+        const profile = await fetchProfile(accessToken);
+        populateUI(profile);
+    }
 }
 
 async function fetchProfile(code: string): Promise<UserProfile> {
